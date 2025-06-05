7b1fcacabf6be0edcac90033ec1a38d5fd0b9b7d
zezva <kobaidze.zezva@kiu.edu.ge>
2025-06-05 19:45:17 +0400

    Initial commit project setup

---
+++ frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vitest/config'; // <-- use vitest/config instead of vite
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
+});
